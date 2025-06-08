6c25bcf98814575297d42c149329237d3ea3d467
pazetivchik01 <romanenkodenisandrwivi@gmail.com>
2025-06-08 13:46:20 +0300

    update DeviceController

--- Controllers/DeviceController.cs
+++ Controllers/DeviceController.cs
@@ -4,25 +4,33 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/[controller]")]
 public class DeviceController : ControllerBase
 {
-    [HttpPost("register")]
-    public IActionResult RegisterDevice([FromBody] DeviceRegistrationRequest request)
+    [HttpPost("register")] // http://localhost:5131/api/device/register
+    public IActionResult RegisterDevice([FromBody] DeviceRequest request)
     {
         if (string.IsNullOrEmpty(request.Token))
             return BadRequest("Token is required");
 
         return Ok($"Device: {request.Device}, with token: {request.Token} succefuly registered"); // регистрация токена
     }
-    [HttpPost("update-token")]
-    public IActionResult update_token([FromBody] DeviceRegistrationRequest request)
+    [HttpPut("update-token")] // http://localhost:5131/api/device/register
+    public IActionResult update_token([FromBody] DeviceRequest request)
     {
         if (string.IsNullOrEmpty(request.Token))
             return BadRequest("Token is required");
 
         return Ok($"Token: {request.Token}, device: {request.Device}, succefuly updated"); // обнавление токена
     }
+    [HttpDelete("delete")] // http://localhost:5131/api/device/delete
+    public IActionResult delete_token([FromBody] DeviceRequest request)
+    {
+        if (string.IsNullOrEmpty(request.Token))
+            return BadRequest("Token is required");
+
+        return Ok($"Token: {request.Token}, device: {request.Device}, succefuly deleted"); // удаление токена
+    }
 }
 
-public class DeviceRegistrationRequest
+public class DeviceRequest
 {
     public string Token { get; set; }
     public string Device { get; set; }
