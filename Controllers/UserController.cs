39e6b83dc461cbc0f7953863e7c2664d9a3008e9
pazetivchik01 <romanenkodenisandrwivi@gmail.com>
2025-06-08 14:36:07 +0300

    add multiple registration

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,12 +13,19 @@ public class UserController : ControllerBase
         return Ok($"User id: {id} succefuly returned"); // получение user-a
     }
     [HttpPost("register")] // http://localhost:5131/api/user/register
-    public IActionResult RegisterUser([FromBody] UserRequest request)
+    public IActionResult RegisterUser([FromBody] List<UserRequest> requests) // регистрация пользователя (json отправлять в виде массива даже если один элемент)
     {
-        if (request == null)
-            return BadRequest("Error accepting data, data is null");
+        if (requests == null || !requests.Any())
+            return BadRequest("No users provided");
 
-        return Ok($"User: {request.lastName} {request.firstName} {request.surnameName} succefuly registered"); // регистрация user-a
+        
+        var response = new List<string>();
+        foreach (var request in requests)
+        {
+            response.Add($"User {request.lastName} {request.firstName} registered");
+        }
+
+        return Ok(response);
     }
     [HttpPut("update")] // http://localhost:5131/api/user/update
     public IActionResult update_user([FromBody] UserRequest request)
@@ -26,7 +33,7 @@ public class UserController : ControllerBase
         if (request == null)
             return BadRequest("Error accepting data, data is null");
 
-        return Ok($"User: {request.lastName} {request.firstName} {request.surnameName} succefuly updated"); // обнавление user-a
+        return Ok($"User: {request.lastName} {request.firstName} {request.surname} succefuly updated"); // обнавление user-a
     }
     [HttpDelete("delete")] // http://localhost:5131/api/user/delete
     public IActionResult delete_user([FromBody] UserRequest request)
@@ -34,18 +41,18 @@ public class UserController : ControllerBase
         if (request == null)
             return BadRequest("Error accepting data, data is null");
 
-        return Ok($"User: {request.lastName} {request.firstName} {request.surnameName} succefuly deleted"); // удаление user-a
+        return Ok($"User: {request.lastName} {request.firstName} {request.surname} succefuly deleted"); // удаление user-a
     }
 }
 
-public class UserRequest
+public class UserRequest // json отправлять в соответствии с пордком полей в классе и своответствии с названиями полей в классе
 {
     public int id { get; set; }
-    public string lastName { get; set; }
+    public string? lastName { get; set; }
     public string firstName { get; set; }
-    public string surnameName { get; set; }
+    public string surname { get; set; }
     public string login { get; set; }
     public string password { get; set; }
     public string role { get; set; }
-    public int createdBy { get; set; }
+    public int? createdBy { get; set; }
 }
