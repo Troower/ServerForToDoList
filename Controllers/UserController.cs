71fca3d732a208784ca83448abdb784c8407e0ee
pazetivchik01 <romanenkodenisandrwivi@gmail.com>
2025-06-08 18:42:40 +0300

    fix UserRequest class

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,16 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
 
 [ApiController]
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
     [HttpGet("get/{id}")] // http://localhost:5131/api/user/get/number (number - это id)
-    public IActionResult GetUser(int id)
+    public IActionResult GetUserByID(int id)
     {
         if(id <= 0)
             return BadRequest("Id is invalid");
 
         return Ok($"User id: {id} succefuly returned"); // получение user-a
+    }
+    [HttpGet("getAll")] // http://localhost:5131/api/user/getAll 
+    public IActionResult GetAllUser()
+    {
+        try 
+        {
+            // Логика получения пользователей
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest("An unknown error occurred");
+        }
     }
     [HttpPost("register")] // http://localhost:5131/api/user/register
     public IActionResult RegisterUser([FromBody] List<UserRequest> requests) // регистрация пользователя (json отправлять в виде массива даже если один элемент)
@@ -33,7 +47,7 @@ public class UserController : ControllerBase
         if (request == null)
             return BadRequest("Error accepting data, data is null");
 
-        return Ok($"User: {request.lastName} {request.firstName} {request.surname} succefuly updated"); // обнавление user-a
+        return Ok($"User: {request.lastName} {request.firstName} {request.surname} succefuly updated"); // обновление user-a
     }
     [HttpDelete("delete")] // http://localhost:5131/api/user/delete
     public IActionResult delete_user([FromBody] UserRequest request)
@@ -45,14 +59,22 @@ public class UserController : ControllerBase
     }
 }
 
-public class UserRequest // json отправлять в соответствии с пордком полей в классе и своответствии с названиями полей в классе
+public class UserRequest // json отправлять в соответствии с пордком полей в классе
 {
+    [JsonPropertyName("user_id")]
     public int id { get; set; }
+    [JsonPropertyName("last_name")]
     public string? lastName { get; set; }
+    [JsonPropertyName("first_name")]
     public string firstName { get; set; }
+    [JsonPropertyName("surname")]
     public string surname { get; set; }
+    [JsonPropertyName("login")]
     public string login { get; set; }
+    [JsonPropertyName("password_hash")]
     public string password { get; set; }
+    [JsonPropertyName("role")]
     public string role { get; set; }
+    [JsonPropertyName("created_by")]
     public int? createdBy { get; set; }
 }
